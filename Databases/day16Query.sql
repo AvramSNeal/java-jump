53743da4d6c6224184663951f4829573a6cffd65
Avram N <AvramSNeal@gmail.com>
2019-11-05 16:52:50 -0600

    CmtPsh Cmder

--- Databases/day16Query.sql
+++ Databases/day16Query.sql
@@ -72,8 +72,71 @@ CREATE TABLE IF NOT EXISTS countries (
     REGION_ID decimal(10,0) NOT NULL
 );
 */
+/*
 CREATE TABLE IF NOT EXISTS countries (
 	COUNTRY_ID integer NOT NULL UNIQUE AUTO_INCREMENT,
     COUNTRY_NAME varchar(40) NOT NULL,
     REGION_ID decimal(10,0) NOT NULL
 );
+*/
+/*
+CREATE TABLE IF NOT EXISTS job_history (
+	EMPLOYEE_ID decimal(6,0) NOT NULL UNIQUE,
+    START_DATE date NOT NULL,
+    END_DATE date NOT NULL,
+    JOB_ID varchar(10) NOT NULL,
+    DEPARTMENT_ID decimal(4,0) NOT NULL,
+    FOREIGN KEY(job_id) REFERENCES jobs(job_id)
+);
+*/
+# desc job_history;
+/*
+CREATE TABLE IF NOT EXISTS countries (
+	COUNTRY_ID varchar(2),
+    COUNTRY_NAME varchar(40) NOT NULL,
+    REGION_ID decimal(10,0) DEFAULT 0
+);
+/*
+/*
+INSERT INTO  countries VALUES('C1','India',1001);
+select * from countries;
+*/
+/*
+INSERT INTO  countries (country_id,country_name) VALUES('C1','India');
+select * from countries;
+/*
+/*
+CREATE TABLE IF NOT EXISTS country_new
+AS SELECT * FROM countries;
+
+SELECT * from country_new;
+
+SHOW COLUMNS FROM country_new;
+*/
+/*
+INSERT INTO countries VALUES('C1','India',1001),('C2','USA',1007),('C3','UK',1003);
+select * from countries;
+*/
+/*
+INSERT INTO country_new SELECT * FROM countries;
+*/
+/**/
+CREATE TABLE IF NOT EXISTS jobs (
+	JOB_ID integer NOT NULL UNIQUE,
+    JOB_TITLE varchar(35) NOT NULL,
+    MIN_SALARY decimal(6,0)
+);
+
+INSERT INTO jobs VALUES(1001, 'OFFICER',0000);
+/**/
+
+/**/
+
+/**/
+
+/**/
+
+/**/
+
+
+
