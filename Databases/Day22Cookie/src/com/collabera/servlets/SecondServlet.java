273b3c20f2d42440dbeb52a611405e034676a111
Avram N <AvramSNeal@gmail.com>
2019-11-12 13:51:52 -0600

    CmtPsh

---
+++ Databases/Day22Cookie/src/com/collabera/servlets/SecondServlet.java
@@ -0,0 +1,29 @@
+package com.collabera.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name= "SecondServlet", urlPatterns = "/servlet2")
+public class SecondServlet extends HttpServlet{
+	private static final long serialVersionUID = 1L;
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+		try {
+			response.setContentType("text/html");
+			PrintWriter out = response.getWriter();
+			
+			//Getting the value from the hidden field
+			String n = request.getParameter("uname");
+			out.print("Hello " + n);
+			out.close();
+		} catch(Exception e) {System.out.println(e);}
+		
+	}
+	
+}
