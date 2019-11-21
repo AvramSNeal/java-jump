608acc2fc8a80d9ba1bfafde10facb31a7101d6b
Avram N <AvramSNeal@gmail.com>
2019-11-21 13:44:11 -0600

    CmtPsh

--- Java Fundamentals/Day03/src/swap/SwapNumbers.java
+++ Java Fundamentals/Day03/src/swap/SwapNumbers.java
@@ -21,9 +21,23 @@ public class SwapNumbers {
 		x = x + y;
 		y = x - y;
 		x = x - y;
-		System.out.println("After Swapping AGAIN!!!");
+		System.out.println("After Swapping again!");
 		System.out.println("x = " + x);
 		System.out.println("y = " + y);
+		
+		int A = 10;
+		int B = 20;
+		System.out.println("Before Swapping A and B");
+		System.out.println("A = " + A);
+		System.out.println("B = " + B);
+		
+		B=A+B;
+		A=B-A;
+		B=B-A;
+	
+		System.out.println("After Swapping A and B");
+		System.out.println("A = " + A);
+		System.out.println("B = " + B);
 		
 		in.close();
 	}
