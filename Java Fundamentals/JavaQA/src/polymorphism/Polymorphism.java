ad8bcf57632798ecedccbfd8db9cdf97adc7efd1
Avram N <AvramSNeal@gmail.com>
2019-11-28 14:00:39 -0600

    CmtPsh

--- Java Fundamentals/JavaQA/src/polymorphism/Polymorphism.java
+++ Java Fundamentals/JavaQA/src/polymorphism/Polymorphism.java
@@ -3,3 +3,80 @@ package polymorphism;
 public class Polymorphism {
 
 }
+//Java program for Method overloading 
+
+class MultiplyFun { 
+
+	// Method with 2 parameter 
+	static int Multiply(int a, int b) 
+	{ 
+		return a * b; 
+	} 
+
+	// Method with the same name but 2 double parameter 
+	static double Multiply(double a, double b) 
+	{ 
+		return a * b; 
+	} 
+} 
+
+class Main { 
+	public static void main(String[] args) 
+	{ 
+
+		System.out.println(MultiplyFun.Multiply(2, 4)); 
+
+		System.out.println(MultiplyFun.Multiply(5.5, 6.3)); 
+	} 
+} 
+//Java program for Method overloading 
+
+class MultiplyFun2 { 
+
+	// Method with 2 parameter 
+	static int Multiply(int a, int b) 
+	{ 
+		return a * b; 
+	} 
+
+	// Method with the same name but 3 parameter 
+	static int Multiply(int a, int b, int c) 
+	{ 
+		return a * b * c; 
+	} 
+} 
+
+class Main2 { 
+	public static void main(String[] args) 
+	{ 
+		System.out.println(MultiplyFun2.Multiply(2, 4)); 
+
+		System.out.println(MultiplyFun2.Multiply(2, 7, 3)); 
+	} 
+} 
+//Java program for Operator overloading 
+
+class OperatorOVERDDN { 
+
+	void operator(String str1, String str2) 
+	{ 
+		String s = str1 + str2; 
+		System.out.println("Concatinated String - "
+						+ s); 
+	} 
+
+	void operator(int a, int b) 
+	{ 
+		int c = a + b; 
+		System.out.println("Sum = " + c); 
+	} 
+} 
+
+class Main3 { 
+	public static void main(String[] args) 
+	{ 
+		OperatorOVERDDN obj = new OperatorOVERDDN(); 
+		obj.operator(2, 3); 
+		obj.operator("joe", "now"); 
+	} 
+} 
