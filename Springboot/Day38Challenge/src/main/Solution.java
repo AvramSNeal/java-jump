d8faf05318429f2b7e991f5f56c8d3a2eeae49b6
Avram N <AvramSNeal@gmail.com>
2019-12-15 12:39:15 -0600

    CmtPsh

--- Springboot/Day38Challenge/src/main/Solution.java
+++ Springboot/Day38Challenge/src/main/Solution.java
@@ -3,31 +3,23 @@ package main;
 
 
 
-
 import java.io.*;
 import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
 public class Solution {
 
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
         
-        Scanner sc=new Scanner(System.in);
-        String A=sc.next();
-        String B=sc.next();
-        /* Enter your code here. Print output to STDOUT. */
-
-        System.out.println(A.length()+B.length());
         
-        if(A.compareTo(B)>0)System.out.println("Yes");
-        else System.out.println("No");
-        
-        System.out.println(
-        		A.substring(0, 1).toUpperCase()+A.substring(1).toLowerCase() +
-        		" " +
-        		B.substring(0, 1).toUpperCase()+B.substring(1).toLowerCase());
-        
+        System.out.println(S.substring(start, end));
     }
 }
-
 
 
