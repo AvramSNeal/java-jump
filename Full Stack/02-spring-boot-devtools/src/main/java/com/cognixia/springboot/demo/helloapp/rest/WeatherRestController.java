2ee8fab05d76d0f38ec317706cb5fb3291437c1a
Avram N <AvramSNeal@gmail.com>
2020-01-15 09:57:44 -0600

    CmtPsh

--- Full Stack/02-spring-boot-devtools/src/main/java/com/cognixia/springboot/demo/helloapp/rest/WeatherRestController.java
+++ Full Stack/02-spring-boot-devtools/src/main/java/com/cognixia/springboot/demo/helloapp/rest/WeatherRestController.java
@@ -2,12 +2,8 @@ package com.cognixia.springboot.demo.helloapp.rest;
 
 import java.time.LocalDateTime;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class WeatherRestController {
